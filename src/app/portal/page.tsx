f4213677ebaeefe599a61ed6a9b977ad4b6ff925
Khaniee <roseiloharaoke@gmail.com>
2025-05-17 17:16:46 +0300

    init open door

---
+++ src/app/portal/page.tsx
@@ -0,0 +1,14 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function Portal() {
+  return (
+    <main className='w-full'>
+      <Spline
+        scene="https://prod.spline.design/1fhGEWcZJS8mL0BC/scene.splinecode" 
+        width={1610}
+        height={968}
+      />
+    </main>
+  );
+}
+
