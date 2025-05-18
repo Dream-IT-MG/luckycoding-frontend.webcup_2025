2c0abe01c4b7349a17a9bc3d6374bd33ccdc75c4
Charla-H4h <m.charla@humans4help.com>
2025-05-18 03:25:57 +0300

    feat(login): design

--- src/app/(auth)/login/layout.tsx
+++ src/app/(auth)/login/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { BackgroundBeams } from "../../../components/ui/background-beams";
 
 export const metadata: Metadata = {
   title: "Login",
@@ -11,7 +12,16 @@ export default function LoginLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="bg-slate-100 h-screen flex justify-center items-center">
+    <div 
+    className="h-screen flex justify-center items-center"
+      style={{
+      backgroundImage: "linear-gradient(to right top, #1a345c, #252d51, #292746, #2a223b, #291d31)",
+      }}
+    >
+      {/* Background beams en fond */}
+        <div className="absolute inset-0 z-0 pointer-events-none">
+          <BackgroundBeams />
+        </div>
       {children}
     </div>
   );
