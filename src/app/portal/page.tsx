0aedaa2dacf9e4519e24a54e05e6c3f3f149dd34
Khaniee <roseiloharaoke@gmail.com>
2025-05-18 04:29:06 +0300

    spline hall of fame

--- src/app/portal/page.tsx
+++ src/app/portal/page.tsx
@@ -1,9 +1,14 @@
 import Spline from "@splinetool/react-spline/next";
+import { Suspense } from "react";
 
 export default function Portal() {
   return (
     <main className="w-full">
-      <Spline scene="https://prod.spline.design/1fhGEWcZJS8mL0BC/scene.splinecode" />
+      <Suspense fallback="Waiting for the scene to generate... ">
+        <Spline
+          scene="https://prod.spline.design/zj7EUb2itZu4S8zX/scene.splinecode" 
+        />
+      </Suspense>
     </main>
   );
 }
